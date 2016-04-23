2585ed253a626fccd55418695906abab524ef546
EX master <EXmaster9@users.noreply.github.com>
2016-04-22 20:58:30 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+exports.bannedTerms = ['foodsaver'];
+
+exports.categories = [
+  {
+    // Smile
+    pinId: 0,
+    labels: ['smile', 'happy', 'joyful'],
+    name: 'Smile'
+  },
+  {
+    // Frown
+    pinId: 1,
+    labels: ['frown', 'sad', 'unhappy'],
+    name: 'Frown'
+  },
+  {
+    // Laugh
+    pinId: 2,
+    labels: ['laugh', 'amused', 'funny', 'laughing'],
+    name: 'Laugh'
+  },
+  {
+    // Tongue
+    pinId: 3,
+    labels: ['tongue', 'lol', 'durh', 'dur', 'duh'],
+    name: 'Tongue'
+  },
+  {
+    // Heart
+    pinId: 4,
+    labels: ['love','heart'],
+    name: 'Heart'
+  }
+];
