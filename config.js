a073dcc1253b73600dc3275bacabe9477ad89eaf
EX master <EXmaster9@users.noreply.github.com>
2016-04-22 21:14:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,34 +1,34 @@
-exports.bannedTerms = ['foodsaver'];
+exports.bannedTerms = ['poop', 'pee'];
 
 exports.categories = [
   {
     // Smile
     pinId: 0,
-    labels: ['smile', 'happy', 'joyful'],
+    labels: ['smile', 'happy', 'joyful','joy', 'grin', 'anti frown', ':)'],
     name: 'Smile'
   },
   {
     // Frown
     pinId: 1,
-    labels: ['frown', 'sad', 'unhappy'],
+    labels: ['frown', 'sad', 'unhappy', 'crying', 'cry', 'tears', ':('],
     name: 'Frown'
   },
   {
     // Laugh
     pinId: 2,
-    labels: ['laugh', 'amused', 'funny', 'laughing'],
+    labels: ['laugh', 'amused', 'funny', 'laughing', 'hilarious', 'XD', ':D'],
     name: 'Laugh'
   },
   {
     // Tongue
     pinId: 3,
-    labels: ['tongue', 'lol', 'durh', 'dur', 'duh'],
+    labels: ['tongue', 'lol', 'durh', 'dur', 'duh', 'doy', ':P'],
     name: 'Tongue'
   },
   {
     // Heart
     pinId: 4,
-    labels: ['love','heart'],
+    labels: ['love','heart', '<3'],
     name: 'Heart'
   }
 ];
