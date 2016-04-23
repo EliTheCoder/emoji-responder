c913cd75eeab06408964503705aae4e4bcdec3ce
Joe Longstreet <joelongstreet@gmail.com>
2016-04-23 17:21:07 -0500

    add body parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express       = require('express');
 var app           = express();
 var server        = require('http').createServer(app);
+var bodyParser    = require('body-parser');
 
 server.listen(3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   res.send('hello there!');
